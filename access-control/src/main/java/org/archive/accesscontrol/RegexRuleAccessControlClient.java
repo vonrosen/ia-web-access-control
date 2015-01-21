ca69a3d59f6276a1e1762517bf1457a508028824
Hunter Stern <hunter@archive.org>
2015-01-20 16:44:23 -0800

    More changes for ARI-3995

--- access-control/src/main/java/org/archive/accesscontrol/RegexRuleAccessControlClient.java
+++ access-control/src/main/java/org/archive/accesscontrol/RegexRuleAccessControlClient.java
@@ -1,14 +1,7 @@
 package org.archive.accesscontrol;
 
-import java.util.Date;
-
-import org.archive.accesscontrol.model.RegexRuleSet;
-import org.archive.accesscontrol.model.Rule;
 import org.archive.accesscontrol.robotstxt.CachingRobotClient;
 import org.archive.accesscontrol.robotstxt.RobotClient;
-import org.archive.net.PublicSuffixes;
-import org.archive.util.ArchiveUtils;
-import org.archive.util.SURT;
 
 public class RegexRuleAccessControlClient extends AccessControlClient {
     
@@ -21,36 +14,8 @@ public class RegexRuleAccessControlClient extends AccessControlClient {
     }
     
     public RegexRuleAccessControlClient(String oracleUrl, boolean cacheOnce) {
-        this(cacheOnce ? new CacheOnceHttpRegexRulesDao(oracleUrl) : new CachingRegexRuleDao(oracleUrl), new CachingRobotClient());
-    }
-
-    /**
-     * Return the most specific matching rule for the requested document.
-     * 
-     * @param url
-     *            URL of the requested document.
-     * @param captureDate
-     *            Date the document was archived.
-     * @param retrievalDate
-     *            Date of retrieval (usually now).
-     * @param who
-     *            Group name of the user accessing the document.
-     * @return
-     * @throws RuleOracleUnavailableException 
-     */
-    public Rule getRule(String url, Date captureDate, Date retrievalDate,
-            String who) throws RuleOracleUnavailableException {
-        url = ArchiveUtils.addImpliedHttpIfNecessary(url);
-        String surt = SURT.fromURI(url);
-//        PublicSuffixes.reduceSurtToAssignmentLevel(surt)
-        String publicSuffix = PublicSuffixes
-                .reduceSurtToAssignmentLevel(getSurtAuthority(surt));
-
-        RegexRuleSet rules =  (RegexRuleSet)ruleDao.getRuleTree(getScheme(surt) + "(" + publicSuffix);
-
-        Rule matchingRule = rules.getMatchingRule(surt, captureDate,
-                retrievalDate, who);
-        return matchingRule;
+        this(cacheOnce ? new CacheOnceHttpRegexRulesDao(oracleUrl)
+                : new CachingRegexRuleDao(oracleUrl), new CachingRobotClient());
     }
     
 }
